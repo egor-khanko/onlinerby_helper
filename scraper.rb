a537717af9fc46a2d2bd97e4524f940b738b6710
Ivan Leschinsky <vanopiano@gmail.com>
2020-09-28 14:10:42 +0300

    Make ruby look better

--- scraper.rb
+++ scraper.rb
@@ -11,76 +11,78 @@ def get_json
   parsed_json_array = []
   page_number = 0
   last_page = nil
-  creating_file = File.exists?(FILE_PATH)
+  file_exists = File.exists?(FILE_PATH)
 
   loop do
     page_number += 1
     url = 'https://catalog.onliner.by/sdapi/catalog.api/search/hdd?page='+page_number.to_s #generating a URL for a request
 
-
-    unparsed_json = HTTParty.get(url).to_s #getting json from API
+    unparsed_json = HTTParty.get(url).to_s # get json from API
     parsed_json = JSON.parse(unparsed_json)
-    parsed_json_array.push(parsed_json) #making an array of parsed JSONs
+    parsed_json_array.push(parsed_json) # make an array of parsed JSONs
 
-    if last_page.nil? #defining last page i not already defined
-      last_page = parsed_json["page"]["last"]
+    # define last page if not already defined
+    if last_page.nil?
+      last_page = parsed_json.dig('page', 'last')
     end
 
-    if creating_file
-      File.foreach(FILE_PATH){|json_page_string| parsed_json_array.push(JSON.parse(json_page_string))} #converting string to hash
+    if file_exists
+      File.foreach(FILE_PATH) do |json_page_string|
+        # convert string to hash
+        parsed_json_array.push(JSON.parse(json_page_string))
+      end
       break
     else
       File.write(FILE_PATH, unparsed_json + "\n", mode: "a")
     end
 
-    puts "Getting page  № #{page_number}" #notifying the user about the progress
-    if page_number == last_page
-      puts "Parsing is done!"
-      break
-    end
+    # notify the user about the progress
+    puts "Getting page ##{page_number}"
+    break puts "Parsing is done!" if page_number == last_page
   end
 
-  return parsed_json_array
+  parsed_json_array
 end
 
 def scraper
   products = []
   products_cleaned = []
-  products_info = Hash.new()
+  products_info = {}
 
+  # extract products from json into "products" array
+  get_json.each { |value| value["products"].each_with_index { |value, index| products.push(value) } }
 
-  get_json.each{ |value| value["products"].each_with_index{|value, index| products.push(value)}} #extracting products from json into "products" array
+  # makes a new array of products without price
+  products.each { |value| if value["prices"] != nil then products_cleaned.push(value) end }
+  products.each { |value| products_cleaned.push(value) if value["prices"] }
 
-  products.each{|value| if value["prices"] != nil then products_cleaned.push(value) end} #making a new array of products without price
-
-  products_cleaned.each_with_index{ #removing all unnecessary info from array
-    |value, index|
-    if !value["full_name"].match(/[0-9.]{1,4}( )?(TB|GB|Гб|Тб)/).nil?
+  #remove all unnecessary info from array
+  products_cleaned.each_with_index do |value, index|
+    if value["full_name"].match(/[0-9.]{1,4}( )?(TB|GB|Гб|Тб)/)
       products_info[value["full_name"]] = {
         "volume_str" => value["full_name"][/[0-9.]{1,4}( )?(TB|GB|Гб|Тб)/],
-        "price"  => value["prices"]["price_min"]["amount"]
+        "price"  => value.dig('prices', 'price_min', 'amount')
       }
     end
-  }
+  end
 
-  products_info.each{|key, value| #making a float volume in TB from string
+  products_info.each do |key, value| # make a float volume in TB from string
     if products_info[key]["volume_str"].match(/TB|Тб/)
       products_info[key]["volume"] = products_info[key]["volume_str"][/[\d.]{1,}/].to_f
     elsif products_info[key]["volume_str"].match(/GB|Гб/)
-      products_info[key]["volume"] = products_info[key]["volume_str"][/[\d.]{1,}/].to_f/1000
+      products_info[key]["volume"] = products_info[key]["volume_str"][/[\d.]{1,}/].to_f / 1000
     end
-  }
+  end
 
-  products_info.each{|key, value| #calculating value for each drive
-    value["value"] = value["price"].to_f/value["volume"]
-  }
+  products_info.each do |_key, value| # calculate value for each drive
+    value["value"] = (value['price'].to_f / value['volume']).round(2)
+  end
 
-  products_info = products_info.sort_by{|key, value| value["value"] } #sorting array by value
+  products_info = products_info.sort_by{|_key, value| value['value'] } # sort array by value
 
-  products_info.each{|key, value| #outputing array name and value
-    puts "#{key} at #{value["value"].round(2)} BYN/TB"
-  }
-
+  products_info.each do |key, value| # output array name and value
+    puts "#{key} at #{value['value']} BYN/TB"
+  end
 end
 
 scraper
